6b15b2f310beb3b7a51cade1abee057c8b4a0f00
John Doe <johndoe@email.com>
2024-12-07 08:40:58 -0600

    Render all charts in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import './App.css'
 
+//import the chart components
 import BarChart from './components/BarChart';
 import LineChart from './components/LineChart';
 import ScatterChart from './components/ScatterChart';
@@ -12,6 +13,7 @@ function App() {
   const [chartData, setChartData] = useState(null);
 
 
+
 //Task 4: Fetch Data for Charts
 useEffect(() => {
   fetch('public/financial_data.json')
@@ -19,11 +21,16 @@ useEffect(() => {
     .then((data) => setChartData(data));
 }, []);
 
+
+
+//Task 5: Render Charts in the App
+
 //display loading message
 if (!chartData) {
   return <div>Loading...</div>;
 }
 
+//render the chart components
 return (
   <div style={{ textAlign: 'center' }}>
     <h1>Dynamic Charts with React and Chart.js</h1>
